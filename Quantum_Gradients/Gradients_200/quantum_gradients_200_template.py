5e99fc4e5501455246fbb29608be9c150235795b
Calum <chmail@holk.co.uk>
2021-02-18 10:27:37 +0000

    QG_200

--- Quantum_Gradients/Gradients_200/quantum_gradients_200_template.py
+++ Quantum_Gradients/Gradients_200/quantum_gradients_200_template.py
@@ -42,73 +42,59 @@ def gradient_200(weights, dev):
         return qml.expval(qml.PauliZ(0) @ qml.PauliZ(2))
 
     gradient = np.zeros([5], dtype=np.float64)
-    s = 0.5 * np.pi
-    denom = 4 * np.sin(s) ** 2
-    shift = np.eye(len(weights))
+    hessian = np.zeros([5, 5], dtype=np.float64)
+    
+    
 
     # QHACK #
+    s = np.pi/2
+    forward_values = np.zeros([5], dtype=np.float64)
+    backward_values = np.zeros([5], dtype=np.float64)
+    noshift = circuit(weights)
+    
     def parameter_shift_term(qnode, weights, i):
         shifted = weights.copy()
-        shifted[i] += np.pi/4
+        shifted[i] += s
         forward = qnode(shifted)  # forward evaluation
+        forward_values[i]=forward
 
-        shifted[i] -= np.pi/2
+        shifted[i] -= 2*s
         backward = qnode(shifted) # backward evaluation
-        return 0.5 * ((forward - backward)/(np.sin(np.pi/4)))
+        backward_values[i]=backward
+        return 0.5 * ((forward - backward)/(np.sin(s)))
     
-    def parameter_shift(qnode, params):
+    def get_gradient(qnode, params):
         for i in range(len(params)):
-            gradient[i] = parameter_shift_term(qnode, weights, i)
-        return gradient
-    def hess_gen_results(weights):
-        
-        results = {}
-        import itertools
-        for c in itertools.combinations(range(len(weights)), r=2):
-            if not results.get(c):
-                weights_pp = weights + s * (shift[c[0]] + shift[c[1]])
-                weights_pm = weights + s * (shift[c[0]] - shift[c[1]])
-                weights_mp = weights - s * (shift[c[0]] - shift[c[1]])
-                weights_mm = weights - s * (shift[c[0]] + shift[c[1]])
+            gradient[i] = parameter_shift_term(qnode, params, i)
 
-                f_pp = circuit(weights_pp)
-                f_pm = circuit(weights_pm)
-                f_mp = circuit(weights_mp)
-                f_mm = circuit(weights_mm)
-                results[c] = (f_pp, f_mp, f_pm, f_mm)
+    def get_hessian(qnode, weights):
+        ij = []
+        shifted = weights.copy()
+        for i in range(len(weights)-1):
+            for j in range(1, len(weights)):
+                if i!=j and [j,i] not in ij:
+                    shifted[i] += s
+                    shifted[j] += s
+                    pp = qnode(shifted)
+                    shifted[j] -= 2*s
+                    pm = qnode(shifted)
+                    shifted[i] -= 2*s
+                    mm = qnode(shifted)
+                    shifted[j] += 2*s
+                    mp = qnode(shifted)
+                    hessian[i][j] = (pp + mm - pm - mp)/(4*(np.sin(s)**2))
+                    hessian[j][i] = hessian[i][j]
+                    shifted[i] += s
+                    shifted[j] -= s
+                ij.append([i,j])
         for i in range(len(weights)):
-            if not results.get((i, i)):
-                f_p = circuit(weights + 0.5 * np.pi * shift[i])
-                f_m = circuit(weights - 0.5 * np.pi * shift[i])
-                f = circuit(weights)
-
-                results[(i, i)] = (f_p, f_m, f)
-        return results
-
-
-    def get_hess(weights):
-        results=hess_gen_results(weights)
-        import itertools
-        hessian = np.zeros([5, 5], dtype=np.float64)
-        for c in itertools.combinations(range(len(weights)), r=2):
-            r = results[c]
-            hessian[c] = (r[0] - r[1] - r[2] + r[3]) / denom
-
-        hessian = hessian + hessian.T
-
-        for i in range(len(weights)):
-            r = results[(i, i)]
-            hessian[i, i] = (r[0] + r[1] - 2 * r[2]) / 2
-        return hessian
-
-    gradient=parameter_shift(circuit,weights)
-    hessian=get_hess(weights)
+            hessian[i][i] = (forward_values[i] + backward_values[i] - 2*noshift)/2
     
-        # iterate over dimensions
-        # apply gradient again to every component of the first derivative.
-        
+    get_gradient(circuit,weights)
+    get_hessian(circuit, weights)
 
-                
+    hessian = hessian + 1 - 1
+
     # QHACK #
 
     return gradient, hessian, circuit.diff_options["method"]
